dc94bb538949dbd25e76af9541893391210c2eb9
win11264 <winthitisan@gmail.com>
2021-06-26 23:37:31 +0700

    JS HW FCC

---
+++ JS_Homework/JS_Homework_1.js
@@ -0,0 +1,166 @@
+
+
+// 1. Comment Your JavaScript Code
+
+//
+// in-line comment
+/* multiple-line
+comment*/
+
+// 2. Declare JavaScript Variables
+
+var myName;
+
+// 3. Storing Values with the Assignment Operator
+
+a = 7;
+
+// 4. Assigning the Value of One Variable to Another
+
+b = a;
+
+// 5. Initializing Variables with the Assignment Operator
+
+var a = 9
+
+// 6. Understanding Uninitialized Variables
+
+var a = 5;
+var b = 10;
+var c = "I am a"
+
+// 7. Understanding Case Sensitivity in Variables
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// 8. Add Two Numbers with Javascript
+
+var sum = 10 + 10;
+
+// 9. Subtract
+
+var difference = 45 - 33;
+
+// 10. Multi
+
+var product = 8 * 10;
+
+// 11. Divide
+
+var quotient = 66 / 33;
+
+// 12. In
+
+myVar = ++myVar;
+
+// 13. De
+
+myVar = --myVar;
+
+// 14. 
+
+var myDecimal = 1.2;
+
+// 15.
+
+var product = 2.0 * 2.5;
+
+// 16.
+
+var quotient = 4.4 / 2.0;
+
+// 17.
+
+var remainder = 5 % 3
+
+// 18.
+
+a += 12;
+b += 9;
+c += 7;
+
+// 19.
+
+a -=6;
+b -=15;
+c -=1;
+
+// 20.
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+// 21.
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+// 22.
+
+var myFirstName = "Thitisan";
+var myLastName = "Kawinvasin";
+
+// 23.
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+// 24.
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// 25.
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+
+// 26. Concate
+
+var myStr = "This is the start. "+"This is the end."
+
+// 27.
+
+var myStr = "This is the first sentence. ";
+
+myStr += "This is the second sentence."
+
+// 28.
+
+var myName = "Thitisan";
+var myStr = "My name is" + myName + "and I am well!"
+
+// 29.
+
+var someAdjective = "fun!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// 30.
+
+lastNameLength= lastName.length;
+
+// 31.
+
+firstLetterOfLastName = lastName[0];
+
+// 32.
+
+var thirdLetterOfLastName = lastName[2];
+
+// 33.
+
+var lastLetterOfLastName = lastName[lastName.length - 1];
+
+// 34.
+
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]
+
+// 35.
+
+myStr = "Hello World";
+
+// 36.
+
+var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
